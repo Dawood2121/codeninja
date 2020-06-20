f1e9bcb0bfe521702ae1fc5f2f9e66c50381db5b
Dawood2121 <66462939+Dawood2121@users.noreply.github.com>
2020-06-20 13:15:54 +0530

    Add files via upload
    Here u can learn the code of function in addition

---
+++ function addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+addition();
+main()
+{
+	addition;
+}
+addition()
+{
+	int a=0,b=0,c=0;
+	printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("sum is :%d",a+b+c);
+}
+
